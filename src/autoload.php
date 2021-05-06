c7f9e06199bb51cfcd0159d268d013b18f51efda
Hexydec <will@hrexydec.com>
2021-05-06 11:43:27 +0100

    Cast result of require() to a bool so it returns the correct variable type in autoload.php.

--- src/autoload.php
+++ src/autoload.php
@@ -2,7 +2,7 @@
 declare(strict_types = 1);
 spl_autoload_register(function (string $class) : bool {
 	if ($class == 'hexydec\\tokens\\tokenise') {
-		return require(__DIR__.'/tokenise.php');
+		return (bool) require(__DIR__.'/tokenise.php');
 	}
 	return false;
 });
