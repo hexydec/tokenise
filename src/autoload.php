75c86cd85ee018356d46578b10519c1ca95173fc
Hexydec <will@hexydec.com>
2023-06-05 22:15:43 +0100

    Updated require to be used as a keyword instead of a function.

--- src/autoload.php
+++ src/autoload.php
@@ -2,6 +2,6 @@
 declare(strict_types = 1);
 \spl_autoload_register(function (string $class) : void {
 	if ($class === 'hexydec\\tokens\\tokenise') {
-		require(__DIR__.'/tokenise.php');
+		require __DIR__.'/tokenise.php';
 	}
 });
